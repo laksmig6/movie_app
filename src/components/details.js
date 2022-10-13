a2e1bb0540a18ff985b111fe6273192d94448bb9
laksmig6 <lakshmig@thoughtworks.com>
2022-10-13 10:10:56 +0530

    Add details

--- src/components/details.js
+++ src/components/details.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 const MovieDetails = (props) => {
-  const { Title } = props.movie;
+  const { Title, Plot, Director, Writer, Actors } = props.movie;
   return (
     <div>
       <h2>Details component</h2>
-      <span>{Title}</span>
+      <div className="my-3">{Title}</div>
+      <div>{Plot}</div>
+      <ul className="list-group mt-4">
+        <li className="list-group-item">
+          <span className="fw-bold mr-2">Directors : </span>
+          {Director}
+        </li>
+        <li className="list-group-item">
+          <span className="fw-bold mr-2">Writers : </span>
+          {Writer}
+        </li>
+        <li className="list-group-item">
+          <span className="fw-bold mr-2">Stars : </span>
+          {Actors}
+        </li>
+      </ul>
     </div>
   );
 };
