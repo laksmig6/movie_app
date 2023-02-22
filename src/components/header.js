e599b41b5e627bcc5789422b3b7470acc7d9a27f
laksmig6 <lakshmig@thoughtworks.com>
2023-02-22 17:07:20 +0530

    set online/offline status in header and search bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+    function handleOffline() {
+      setIsOnline(false);
+    }
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    return () => {
+      window.addEventListener("online", handleOnline);
+      window.addEventListener("offline", handleOffline);
+    };
+  }, []);
+
   return (
-    <header className="navbar navbar-expand fixed-top navbar-dark bg-dark bd-navbar">
+    <header className="navbar navbar-expand justify-content-between fixed-top navbar-dark bg-dark bd-navbar">
       <div className="navbar-header">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
@@ -18,6 +35,7 @@ const Header = () => {
           </li>
         </ul>
       </div>
+      <div className="pr-3">{isOnline ? "✅ Online" : "❌ Disconnected"}</div>
     </header>
   );
 };
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SearchBar = (props) => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+    function handleOffline() {
+      setIsOnline(false);
+    }
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
   return (
     <div className="col col-sm-4">
       <input
+        disabled={!isOnline}
         className="form-control"
         type="text"
         data-testid="search-text"
         value={props.search}
-        placeholder="Type to search"
+        placeholder={isOnline ? "Type to search" : "Reconnecting..."}
         onChange={(event) => {
           props.setSearch(event.target.value);
         }}
