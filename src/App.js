1c1838b864438f07673e6028ee442baa3d17d69e
laksmig6 <lakshmig@thoughtworks.com>
2023-02-24 11:15:55 +0530

    Server side rendering - express.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
