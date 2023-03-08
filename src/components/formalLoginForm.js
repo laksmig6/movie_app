d659e5ac69a8035f73f91c995c19060c080c26d6
laksmig6 <lakshmig@thoughtworks.com>
2023-03-08 17:55:56 +0530

    form updated

--- src/components/formalLoginForm.js
+++ src/components/formalLoginForm.js
@@ -6,26 +6,34 @@ const FormalLoginForm = () => {
 
   const validateForm = () => {
     let x = document.forms["loginForm"]["username"].value;
-    if ((x = "")) {
+    if (x === "") {
       alert("name must be filled out");
     }
   };
 
   return (
-    <form name="loginForm" onSubmit={validateForm}>
-      <label for="username">UserName</label>
+    <form
+      className="formal-login mt-5"
+      name="loginForm"
+      onSubmit={validateForm}
+    >
+      <label htmlFor="username">UserName</label>
       <input
         type="text"
         name="username"
         value={username}
-        onChange={setUsername(username)}
+        onChange={(e) => {
+          setUsername(username);
+        }}
       />
-      <label for="password">Password</label>
+      <label htmlFor="password">Password</label>
       <input
         type="password"
         name="password"
         value={password}
-        onChange={setPassword(password)}
+        onChange={() => {
+          setPassword(password);
+        }}
       ></input>
       <input type="submit" value="Submit"></input>
     </form>
